c1f7a5e9e117311dbdb85a22473586e4dce27973
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-11-30 15:24:47 +0500

    create web api project

---
+++ AspCoreWebApi/Controllers/FruitsApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsApiController : ControllerBase
+    {
+        List<string> fruits = new List<string>
+        {
+            "Apple",
+            "Banana",
+            "Cherry",
+            "Date",
+            "Elderberry"
+        };
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+
+        }
+    }
+
+   
+ }
